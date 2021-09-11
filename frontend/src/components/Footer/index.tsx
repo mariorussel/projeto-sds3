7d48b6fba25e41ec6a13b35dd733a8734b3e3fa7
Mario Russel <mario.russel@gmail.com>
2021-09-11 16:22:13 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/mariorussel" target="_blank" rel="noreferrer">Mario Russel</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <p className="text-light"><small>Meu primeiro app: <a href="https://instagram.com/mario.russel" target="_blank" rel="noreferrer">@mario.russel</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
